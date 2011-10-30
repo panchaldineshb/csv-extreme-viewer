bb0f29a6bc43516c69d5175b5ec69faa41ae77c9
a01130939 <a01130939@03ecf112-8854-b629-f308-d5dcf35f3c84>
2011-10-30 23:31:45 +0000

    Added source files to the source folder.

---
+++ source/CSV Xtreme Viewer/CSV Xtreme Viewer/Loader/LoaderListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSVXtremeLoader
+{
+    interface LoaderListener
+    {
+        void OnStatisticsChanged(CSVStatistics statistics);
+        void OnNewLinesAvailable(CSVStatistics statistics);
+        void OnCorruptedFile(string message);
+    }
+}
