7f646a634ff15b97ff51ecf7fe6805799c795932
a01130939 <a01130939@03ecf112-8854-b629-f308-d5dcf35f3c84>
2011-10-30 23:31:45 +0000

    Added source files to the source folder.

---
+++ trunk/source/CSV Xtreme Viewer/CSV Xtreme Viewer/Filters/Filter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSVXtremeLoader.Structures;
+using System.Text.RegularExpressions;
+
+namespace CSVXtremeLoader
+{
+    abstract class IFilter
+    {
+        protected Regex columnPattern = new Regex("\\G([ ]*\"(?<t>(.|\\\\\")*?)\"[ ]*|(?<t>.*?))(,|$)", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+
+        public abstract bool IsLineValid(String rawLine, Metadata metadata);
+
+        protected Line ParseLine(string rawLine, Metadata metadata)
+        {
+            int columnsCount = metadata.ColumnsCount;
+            Line line = new Line(columnsCount);
+
+            // Parse the line
+            int count = 0;
+            Match match = columnPattern.Match(rawLine);
+            while (match.Success)
+            {
+                line.columns[count] = match.Groups[1].Value.Trim();
+                count++;
+                match = match.NextMatch();
+                if (count >= columnsCount) break;
+            }
+
+            return line;
+        }
+
+    }
+}
---
+++ trunk/source/ConsoleApplication2/getData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class getData
+    {
+        public int RandomNumber(int max)
+        {
+            Random random = new Random(DateTime.UtcNow.Millisecond );
+            return random.Next(max);       
+
+        }
+
+        public string Get_Data(String[] arreglo)
+        {
+            return arreglo[RandomNumber(arreglo.Length)];
+        }
+    }
+}
