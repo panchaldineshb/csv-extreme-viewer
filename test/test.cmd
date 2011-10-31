5b06d82dac3e0cafadbca961d9e651cd2e62eea9
jorge.new.age@gmail.com <jorge.new.age@gmail.com@03ecf112-8854-b629-f308-d5dcf35f3c84>
2011-10-31 01:41:07 +0000

    Update tests

---
+++ test/test.cmd
@@ -0,0 +1,5 @@
+utils\nuint\nunit-console-x86.exe /labels /noshadow /exclude:/timeout:1500000 /out:TestResult.txt /err:TestErr.txt /xml:TestResult.xml  //nutint
+Tests\Test\bin\Debug\TraceDecodeIntegrationTests.dll //dll test
+
+
+
