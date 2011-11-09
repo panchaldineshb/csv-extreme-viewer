b997f947931e4faa406ed13bd2c1ba24429d6da3
jdelrios@gmail.com <jdelrios@gmail.com@03ecf112-8854-b629-f308-d5dcf35f3c84>
2011-11-09 02:52:58 +0000

    Capacidad de guardar archivos.

---
+++ source/CSVXtremeLoader2/CSVLoader/Saver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace CSVLoader
+{
+    public class Saver
+    {
+        private StreamWriter writer;
+        private LineReader lineReader;
+        private CSVStatistics statistics;
+        private Thread saverThread;
+
+        public long CurrentLine;
+        public long TotalLines;
+
+        public Saver(string filename, LineReader lineReader, CSVStatistics statistics)
+        {
+            writer = new StreamWriter(File.Create(filename));
+            this.lineReader = lineReader;
+            this.statistics = statistics;
+            TotalLines = statistics.LinesCount;
+        }
+
+        public void Start()
+        {
+            saverThread = new Thread(SaveProcess);
+            saverThread.Name = "Saver Thread";
+            saverThread.Start();
+        }
+
+        private void SaveProcess()
+        {
+            for (CurrentLine = 0; CurrentLine < statistics.FilteredLines; CurrentLine++)
+            {
+                string line = lineReader.GetRawLine(CurrentLine);
+                writer.WriteLine(line);
+            }
+            writer.Close();
+        }
+
+    }
+}
---
+++ source/CSVXtremeLoader2/CSVXtremeViewer/SaverDialog.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace CSVXtremeViewer
+{
+    partial class SaverDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 25);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(430, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Saving...";
+            // 
+            // SaverDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 96);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar);
+            this.Name = "SaverDialog";
+            this.Text = "Save";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+    }
+}
